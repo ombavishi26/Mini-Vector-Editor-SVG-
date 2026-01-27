452c63f48d676247456f010826f0444385ab8b30
ombavishi26 <ombavishi26@gmail.com>
2026-01-27 19:47:49 +0000

    add path.cpp and .h file and write till to_svg function

---
+++ Objects/path.cpp
@@ -0,0 +1,44 @@
+#include "path.h"
+namespace def = Default;
+namespace defpath = Default::path;
+using obj = GraphicsObject;
+
+//constructor
+path::path() : GraphicsObject(), d(defpath::d) {};
+path::path(const std::string& d, const std::string& stroke, const std::string& fill, const float sw) 
+           : GraphicsObject(stroke, fill, sw), d(d) {};
+//destructor
+path::~path() {};
+//print properties
+void path::print_properties() const {
+    std::cout << d << " ";
+    GraphicsObject::print_properties();   
+}
+//to svg return the svg syntax for path
+std::string path::to_svg() const {
+    std::string str = "<path ";;
+    if (d != defpath::d){
+        str += "d=\"" +  d + "\" ";
+    }
+    if (obj::get_stroke() != def::stroke){
+        str += "stroke=\"" + obj::get_stroke() + "\" ";
+    }
+    if (obj::get_fill() != def::fill){
+        str += "fill=\"" + obj::get_fill() + "\" "; 
+    }
+    if (obj::get_strokewidth() != def::strokewidth){
+        str += "stroke-width=\"" + std::to_string(obj::get_strokewidth()) + "\" ";
+    }
+    str += "/>";
+    return str;
+}
+
+int main(){
+    path defuaktpath;
+    path pa("M10 10 H 90 V 90 H 10 L 10 10","red","blue",2.0);
+    defuaktpath.print_properties();
+    pa.print_properties();
+    std::cout << defuaktpath.to_svg() << std::endl;
+    std::cout << pa.to_svg() << std::endl;
+    return 0;
+}
---
+++ Objects/path.h
@@ -0,0 +1,17 @@
+#include "GraphicsObject.h"
+
+class path : public GraphicsObject {
+private:
+    std::string d;
+
+public:
+    //constructor
+    path();
+    path(const std::string& d, const std::string& stroke = Default::stroke, const std::string& fill = Default::fill, const float sw = Default::strokewidth);
+    //destructor
+    ~path();
+    //print properties
+    void print_properties() const;
+    //to svg    
+    std::string to_svg() const override;
+};
