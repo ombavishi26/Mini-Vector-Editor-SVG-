06a7c5cb3e21166849bf8401ae9de051df8da1ee
ombavishi26 <ombavishi26@gmail.com>
2026-01-27 13:39:32 +0000

    make congif/default.h for default value of objects and added default for rect in it

---
+++ Objects/config/Default.h
@@ -0,0 +1,15 @@
+// #include <string>
+namespace Default{
+    constexpr const char* stroke = "black";
+    constexpr float strokewidth = 1.0f;
+    constexpr const char* fill = "none";
+
+    namespace rect{
+        constexpr float width = 10.0;
+        constexpr float height = 10.0;
+        constexpr float x = 0.0;
+        constexpr float y = 0.0;
+        constexpr float rx = 0.0;
+        constexpr float ry = 0.0;
+    } 
+}
