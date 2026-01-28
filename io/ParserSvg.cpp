98eb370b872e131a46c3ea9bbb564c4737bcb65f
ombavishi26 <ombavishi26@gmail.com>
2026-01-28 17:47:16 +0000

    add seperator function which output vector of pair of property and value for the class

--- io/ParserSvg.cpp
+++ io/ParserSvg.cpp
@@ -16,6 +16,33 @@ void parser_initial(const std::string& line, int i){
     }
 }
 
+std::vector<std::pair<std::string,std::string>> seperate (const std::string& line,int pos , int i){
+    std::vector<std::pair<std::string,std::string>> properties;
+    while (line.substr(pos,2) != "/>"){
+        std::pair<std::string,std::string> attribute;
+        //finding pos of = so between is property
+        int pos_equal = line.find('=',pos);
+        std::string property = line.substr(pos,pos_equal-pos);
+        attribute.first = property;
+        // std::cout << property << "aaa" << std::endl;
+        //finding pos of second " for value of property
+        int pos_quote = line.find('"',pos_equal+2);
+        std::string value = line.substr(pos_equal+2,pos_quote -(pos_equal+2));
+        // std::cout << value << "aaa" <<std::endl;
+        attribute.second = value;
+        properties.push_back(attribute);
+        pos = pos_quote+2;
+    }
+}
+
+void parser_circle(const std::string& line,int pos,int i){
+    std::vector<std::pair<std::string,std::string>> properties;
+    properties = seperate (line,pos,i); 
+    for(auto attribute : properties){
+        
+    }
+}
+
 std::vector<GraphicsObject*> load_svg (const std::string& filename){
     std::ifstream file(filename);
     std::vector<GraphicsObject*> objects;
@@ -35,10 +62,23 @@ std::vector<GraphicsObject*> load_svg (const std::string& filename){
     parser_initial(line,err_index);
 
     //do something till line is not last line ehich is </svg>
+    std::getline(file,line);  //to next line
+    err_index +=1;
     while (line != "</svg>"){
-        std::getline(file,line);
-
+        
+        if(line.at(0) != '<'){
+            std::cerr << "Error at line " << err_index << ": line musst start with '<' ";
+        }
+        int pos_s = line.find(' ');
+        std::string obj_type = line.substr(1,pos_s-1);
         std::cout << line << std::endl;
+        std::cout << obj_type << std::endl;
+        if (obj_type == "circle"){
+            parser_circle(line,pos_s+1,err_index);
+        }
+
+        std::getline(file,line);
+        err_index ++;
     }
 
     file.close();
--- io/ParserSvg.h
+++ io/ParserSvg.h
@@ -9,4 +9,12 @@
 //parsing initiall line 
 void parser_initial(const std::string& line, int i);
 
+//function which takes input line , postion and gives a vector of pairs in which there are properties in string and their values in pair
+std::vector<std::pair<std::string,std::string>> seperate (const std::string& line,int pos ,int i);
+
+
+void parser_rect(const std::string& line,int pos, int i);
+//parsing circle line and gi
+void parser_circle(const std::string& line , int pos, int i);
+
 std::vector<GraphicsObject*> load_svg (const std::string& filename);
