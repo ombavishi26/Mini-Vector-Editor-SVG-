12483cb57e1041c016cfcf3fcdcc92069c695267
ombavishi26 <ombavishi26@gmail.com>
2026-01-28 15:42:45 +0000

    add svgsaver - it will take vector<objects> and give .svg file . add Make file which link all object.o file to this dir

---
+++ io/SvgSaver.cpp
@@ -0,0 +1,28 @@
+#include "SvgSaver.h"
+
+void svg_saver(const std::string& filename, std::vector<GraphicsObject*>& objects){
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error:Could not open file " << filename << std::endl;
+        return;
+    }
+    file << "<svg width=\"200\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\">" << std::endl;
+    // std::vector<GraphicsObject*> objects_copy = objects;
+    objects.erase(std::remove(objects.begin(),objects.end(), nullptr), objects.end());
+    for (int i = 0; i < objects.size(); ++i){
+        file << objects[i]->to_svg() << std::endl;
+    }
+    file << "</svg>" << std::endl;
+    file.close();
+}
+
+int main(){
+    std::vector<GraphicsObject*> objects;
+    objects.push_back(new rect());
+    objects.push_back(new circle(30.0,50.0,50.0,"red","blue",2.0));
+    objects.push_back(new line(10.0,10.0,100.0,100.0,"green",1.5));
+    objects.push_back(new path("M10 10 H 90 V 90 H 10 L 10 10","purple","yellow",2.0));
+    objects.push_back(new text(20.0,30.0,0.0,0.0,0.0,14,"Sample Text","black","none",1.0));
+    svg_saver("output.svg",objects);
+    return 0;
+}
--- io/SvgSaver.h
+++ io/SvgSaver.h
@@ -1,5 +1,8 @@
-#include "Object.h"
+#pragma once
+#include "../Objects/Object.h"
 #include <string>
 #include <vector>
+#include <fstream>
+#include <algorithm>
 
 void save_svg(const std::string& filename, const std::vector<GraphicsObject*>& objects);
