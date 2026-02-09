617e902988e199a4bc1dfb28ea7fdd157226366d
ombavishi26 <ombavishi26@gmail.com>
2026-02-09 22:54:41 +0000

    add menubar and toolbar with no action

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -5,4 +5,16 @@ MainWindow::MainWindow() {
     resize (800, 600);
     canvas = new QWidget(this);
     setCentralWidget(canvas);
+
+    fileMenu = menuBar() -> addMenu("File");
+    fileMenu ->addAction("New");
+    fileMenu ->addAction("Open");
+    fileMenu ->addAction("Save");
+    fileMenu ->addAction("Save As");
+    fileMenu ->addAction("Close");
+    menuBar() -> addAction("Edit");
+
+    toolbar = new QToolBar("tools", this);
+    addToolBar(Qt::LeftToolBarArea, toolbar);
+    toolbar -> addAction("Rectangle");
 }
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -2,11 +2,14 @@
 #include <QMainWindow>
 #include <QToolBar>
 #include <QWidget>
+#include <QMenuBar>
+#include <QMenu>
 
 class MainWindow : public QMainWindow {
 private:
     QWidget* canvas;
     QToolBar* toolbar;
+    QMenu* fileMenu;
 
 public:
     MainWindow();
